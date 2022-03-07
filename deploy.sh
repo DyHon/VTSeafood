a5014a18f1abdaa480c1a1540a8e6e649d539a25
DyHon1 <lehoang8d@gmail.com>
2022-03-07 07:52:02 +0700

    deploy sv

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ cd dist
 git init
 git add -A
 git commit -m 'New deployment'
-git push -f git@github.com:DyHon/SeafoodWebApp.git master:gh-pages
+git push -f git@github.com:DyHon/VTSeafood.git master:gh-pages
 
 cd -
